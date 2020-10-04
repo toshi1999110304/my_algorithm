24adde53d9314f3aa7eb415c27f5485430dd02ca
toshi1999110304 <66793917+toshi1999110304@users.noreply.github.com>
2020-10-05 01:19:04 +0900

    Update eratosthenes.py

--- eratosthenes.py
+++ eratosthenes.py
@@ -1,10 +1,12 @@
-def eratosthenes(A_max):
-    p_flag = [True] * (A_max + 1)
+# O(AloglogA) 
+# A までの素数を求める
+def eratosthenes(A):
+    p_flag = [True] * (A + 1)
     p_flag[0] = False
     p_flag[1] = False
-    for i in range(2, int(A_max ** 0.5) + 1):
+    for i in range(2, int(A ** 0.5) + 1):
         if p_flag[i]:
-            for j in range(i * 2, A_max + 1, i):
+            for j in range(i * 2, A + 1, i):
                 p_flag[j] = False
                 
     return p_flag # 素数のバイナリリスト
