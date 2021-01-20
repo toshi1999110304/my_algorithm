69a77d52a3d9891610f7b848aa1404d383e1ca2f
toshi1999110304 <66793917+toshi1999110304@users.noreply.github.com>
2021-01-20 23:17:59 +0900

    Update gamewellness.py

--- gamewellness.py
+++ gamewellness.py
@@ -10,3 +10,18 @@ def nonpara_norm(data):
     data -= box[2]
     data /= box[3] - box[1]
     return data
+
+
+# RRIBLEによって取得したHRVデータからあるイベント発生時から前range_分間を取り出す
+# condition_id : あるイベントのid
+# range_ : 取り出す長さ（分）
+def hrv_split(data, condition_id, range_):
+    start = data[data['condition_id'] == condition_id]['Current Time']
+    s = start.values[0][11:19]
+    first = start.index.values[0]
+    new_index = first - 1
+    new = data.iloc[new_index, 0][11:19]
+    while int(s[:2]) * 60 * 60 + int(s[3:5]) * 60 + int(s[6:]) - (int(new[:2]) * 60 * 60 + int(new[3:5]) * 60 + int(new[6:])) < range_ * 60:
+        new = data.iloc[new_index, 0][11:19]
+        new_index -= 1
+    return data.iloc[new_index:first, :]
